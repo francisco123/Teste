bd0d513ecbf30fbdc8bfada263e805629aa8e733
francisco123 <francisco_psn@hotmail.com>
2014-03-11 22:31:04 -0300

    Alterando Helloworld para HelloYou

--- Testando/src/Teste.java
+++ Testando/src/Teste.java
@@ -1,7 +1,11 @@
 import static java.lang.System.out;
+import java.util.Scanner;
 public class Teste {
 
 	public static void main(String[] args){
-		out.println("HelloWorld");
+		Scanner entrada=new Scanner(System.in);
+		out.println("Digite seu nome: ");
+		int nome=entrada.nextInt();
+		out.println("Hello, "+ nome);
 	}
 }
